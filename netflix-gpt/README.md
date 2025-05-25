6370f75cdb58922391b7af96c6ee2097432aec10
Kunal Parihar <kunal.parihar2494@gmail.com>
2025-05-25 08:34:48 +0530

    cra and tailwind setup

---
+++ netflix-gpt/README.md
@@ -0,0 +1,15 @@
+# Netflix Gpt
+
+-created reactr app
+-configured tailwind css
+
+# Features
+Login sign in/sign up page
+ - 
+-Browse (after authentication)
+    -Header
+    -Main Movie
+        -Trailer
+        -Title & Description
+        -Movie Suggestion
+          
---
+++ netflix-gpt/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+<div className="container mx-auto">
+      <h1 className="text-3xl font-bold text-blue-500">Hello, Tailwind!</h1>
+      <p className="text-gray-700">This is a simple React app using Tailwind CSS.</p>
+    </div>
+  );
+}
+
+export default App;
