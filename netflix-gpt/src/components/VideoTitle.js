083b639b3ab0d9e937da993358c3cc7d71ac1a58
Kunal Parihar <kunal.parihar2494@gmail.com>
2025-06-04 11:44:04 +0530

    firebase confg

--- netflix-gpt/src/components/VideoTitle.js
+++ netflix-gpt/src/components/VideoTitle.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="pt-[30%] md:pt-[20%] pl-[5%] h-screen w-screen aspect-video absolute text-white bg-gradient-to-r from-black">
+    <div className="pt-[30%] md:pt-[15%] pl-[5%] h-screen w-screen aspect-video absolute text-white bg-gradient-to-r from-black">
       <h1 className="text-3xl md:text-6xl font-bold">{title}</h1>
       <p className="py-6 text-lg md:w-1/5">{overview}</p>
       <div>
